be599beec2e2dd51a028b9646bfdfb8fe5a75b8f
Olinka <olga.titzenthaler@gmail.com>
2024-02-01 00:31:49 +0100

    Added Sun in MainProgram.java Pig still missing

--- src/main/java/cz/czechitas/java/homework01/MainProgram.java
+++ src/main/java/cz/czechitas/java/homework01/MainProgram.java
@@ -1,56 +1,40 @@
-package cz.czechitas.java.ukol01;
+package cz.czechitas.java.homework01;
 
-import cz.czechitas.java.ukol01.engine.Turtle;
+import cz.czechitas.java.homework01.engine.Turtle;
 
 import java.awt.*;
 
-public class HlavniProgram {
+public class MainProgram {
   private Turtle zofka;
 
   public static void main(String[] args) {
-    new HlavniProgram().start();
+    new MainProgram().start();
   }
 
   public void start() {
     zofka = new Turtle();
+    zofka.setSpeed(100);
 
     //TODO implementace domácího úkolu
 
-    zofka.penUp();
-    zofka.turnLeft(90);
+    zofka.setX(500);
+    zofka.setY(150);
 
-    zofka.move(400);
-    zofka.turnRight(90);
-
-    drawBlueLineRights(150);
-    drawCircle(100);
+    drawSun(40);
 
 
-    zofka.turnRight(90);
-    zofka.move(100);
-    zofka.turnLeft(90);
+    zofka.setX(100);
+    zofka.setY(400);
 
-    drawBlueLineRights(150);
+    for (int i = 0; i < 5; i++) {
+      drawHouse();
+      zofka.turnRight(90);
+      zofka.move(30);
+      zofka.turnLeft(90);
+    }
 
-    drawCircle(100);
-
-    zofka.turnRight(90);
-    zofka.move(100);
-    zofka.turnLeft(90);
-
-    drawBlueLineRights(150);
-
-    drawBlueLineAtAngle(100, 0);
-    drawBlueLineLefts(100);
-    drawBlueLineAtAngle(116, 46.60282F);
-    drawBlueLineLefts(282);
-    drawBlueLineAtAngle(116, 133.60219F);
-    drawBlueLineLefts(100);
-    drawBlueLineAtAngle(100, 180);
-
-    zofka.turnLeft(180);
-    zofka.move(250);
-    zofka.turnLeft(180);
+    zofka.setX(100);
+    zofka.setY(600);
 
     drawHouse();
 
@@ -58,16 +42,26 @@ public class HlavniProgram {
     zofka.move(30);
     zofka.turnLeft(90);
 
-
-    writeO();
-    writeL();
-    writeG();
-    writeA();
+    writeOLGA();
 
     zofka.turnRight(90);
     zofka.move(30);
     zofka.turnLeft(90);
 
+    drawHouse();
+
+    zofka.setX(100);
+    zofka.setY(600);
+
+
+
+  }
+
+  public void writeOLGA() {
+    writeO();
+    writeL();
+    writeG();
+    writeA();
   }
 
 
@@ -154,33 +148,6 @@ public class HlavniProgram {
     zofka.turnRight(180);
   }
 
-  public void drawBlueLineRights(int delka) {
-    zofka.penDown();
-    zofka.turnRight(90);
-    zofka.setPenColor(Color.blue);
-    zofka.move(delka);
-    zofka.turnLeft(90);
-    zofka.penUp();
-  }
-
-  public void drawBlueLineLefts(int delka) {
-    zofka.penDown();
-    zofka.turnLeft(90);
-    zofka.setPenColor(Color.blue);
-    zofka.move(delka);
-    zofka.turnRight(90);
-    zofka.penUp();
-  }
-
-  public void drawBlueLineAtAngle(int delka, float uhel) {
-    zofka.penDown();
-    zofka.setPenColor(Color.blue);
-    zofka.turnLeft(uhel);
-    zofka.move(delka);
-    zofka.turnRight(uhel);
-    zofka.penUp();
-  }
-
   public void drawGrayLineAtAngle(float delka, float uhel) {
     zofka.penDown();
     zofka.setPenColor(Color.darkGray);
@@ -190,18 +157,35 @@ public class HlavniProgram {
     zofka.penUp();
   }
 
-
-  public void drawCircle(int diameter) {
-    diameter = diameter / 2;
+  //NEEDS 2B FINISHED
+  public void pigLeg(){
     zofka.penDown();
-    for (int i = 0; i < 360; i++) {
-      zofka.setPenColor(Color.black);
-      zofka.move(2 * Math.PI * diameter / 360);
-      zofka.turnRight(1);
-    }
-    zofka.penUp();
+    zofka.turnRight(60);
+    zofka.move(30);
+    //zofka.penUp();
+    zofka.turnRight(60);
+    zofka.move(30);
+    zofka.penDown();
+    zofka.turnRight(60);
+    zofka.move(30);
+    zofka.turnRight(60);
   }
-  public  void  drawHouse(){
+
+  //NEEDS 2B FINISHED
+    public void drawPig(){
+      zofka.penUp();
+      zofka.turnRight(90);
+      zofka.move(60);
+      drawHouse();
+      zofka.turnLeft(90);
+      zofka.move(100);
+      pigLeg();
+      zofka.turnLeft(90);
+      zofka.move(100);
+      pigLeg();
+
+}
+    public  void  drawHouse(){
     drawSquare();
       zofka.setPenColor(Color.darkGray);
       zofka.penDown();
@@ -224,5 +208,51 @@ public class HlavniProgram {
       zofka.penUp();
     }
 
+  public void drawSun(int sunRadius) {
+    drawSunCircle(sunRadius);
+    drawSunRays(sunRadius);
+  }
 
-}
+  public void drawSunCircle(int radius) {
+    zofka.penUp();
+    zofka.setPenColor(Color.yellow);
+    zofka.move(radius);
+    zofka.penDown();
+
+    for (int i = 0; i < 360; i++) {
+      zofka.move(2 * Math.PI * radius / 360);
+      zofka.turnRight(1);
+    }
+
+    zofka.penUp();
+    zofka.move(-radius);
+  }
+
+  public void drawSunRays(int radius) {
+    zofka.penUp();
+    zofka.move(radius);
+    zofka.turnRight(90);
+    zofka.move(radius);
+    zofka.turnLeft(90);
+    zofka.setPenColor(Color.yellow);
+    zofka.penDown();
+
+    int numRays = 12;
+
+    for (int i = 0; i < numRays; i++) {
+      zofka.penUp();
+      zofka.turnRight(360 / numRays);
+      zofka.move(radius);
+      zofka.penDown();
+      zofka.move(radius);
+      zofka.penUp();
+      zofka.turnLeft(180);
+      zofka.move(2*radius);
+
+    }
+
+  }
+
+
+  }
+
